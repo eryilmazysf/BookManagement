fa0e56da7c908caa6da84f09ae750eb89bc39e30
Yusuf Eryilmaz <yusuferyilmaz1819@gmail.com>
2021-09-06 19:18:52 -0400

    book management project

---
+++ src/main/java/Database.java
@@ -0,0 +1,11 @@
+
+public class Database {
+    
+    
+    //database informations
+    public static final String id = "root";
+    public static final String password="lmlzhop1q.";
+    public static final String host="localhost";
+    public static final String port="3306";
+    public static final String db_name="books_library";
+}
